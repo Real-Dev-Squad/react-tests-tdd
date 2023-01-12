8a007f2dea7e3c39f03f87f8211e6feff5331df3
shubham bajaj <shmbajaj32@gmail.com>
2023-01-12 16:21:05 +0530

    feat: tests added for stepper-form

---
+++ src/components/stepper-form/index.spec.js
@@ -0,0 +1,12 @@
+import StepperForm from "./index.js";
+import { render, screen } from "@testing-library/react";
+
+describe("Perform validations on the input to enable or disable the Next step button", () => {
+  test("it renders stepper-form with inputs, previous and next buttons", () => {});
+  test("it validates initial step or form inputs and enables next step button", () => {});
+  test("it invalidates initial step or form inputs and check is previous button disabled", () => {});
+  test("it renders stepper-form and validates next step or form inputs and enables next step button", () => {});
+  test("it renders stepper-form and invalidates next step or form inputs and disables next step button and check is previous button disabled", () => {});
+  test("it renders transition between forms or steps", () => {});
+  test("it renders and validates stepper form", () => {});
+});
